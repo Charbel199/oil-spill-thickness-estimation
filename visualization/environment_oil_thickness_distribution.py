366d1b8f1587a5887b0feb4c313e564638e9b130
Charbel199 <charbel-boumaroun@outlook.com>
2022-01-15 11:41:03 +0200

    Add fill environment function

--- visualization/environment_oil_thickness_distribution.py
+++ visualization/environment_oil_thickness_distribution.py
@@ -1,5 +1,7 @@
 import numpy as np
 from matplotlib import pyplot as plt
+from data.data_parser import DataParser
+import random
 
 
 def get_circle_thickness_distribution(
@@ -24,6 +26,27 @@ def get_circle_thickness_distribution(
         environment = fill_circle(environment, thickness, radius, center)
         radius -= int(radius_step_size * size)
     return environment
+
+
+def fill_environment_with_reflectivity_data(
+        environment: np.ndarray,
+        data_parser: DataParser
+) -> np.ndarray:
+    populated_environment = []
+    for x in range(len(environment)):
+        temp_populated_environment = []
+        for y in range(len(environment[x])):
+            thickness = environment[x][y]
+            thickness_index = np.where(data_parser.all_thicknesses == thickness)[0]
+            # Get thickness index
+            random_data_point_index = random.randint(0, len(data_parser.all_thicknesses_data[thickness_index]))
+            ref = data_parser.all_thicknesses_data[thickness_index][random_data_point_index]
+            temp_populated_environment.append(ref)
+
+        populated_environment.append(temp_populated_environment)
+
+    populated_environment = np.array(populated_environment)
+    return populated_environment
 
 
 if __name__ == "__main__":
