44fec5359236e17f04f9ea219dc9c29c45f05ae3
Charbel199 <charbel-boumaroun@outlook.com>
2022-03-19 14:12:48 +0200

    Create parent model class and clean up NN and svr model classes

--- model/nn_model.py
+++ model/nn_model.py
@@ -1,61 +1,15 @@
-from sklearn.model_selection import train_test_split
-import numpy as np
-from sklearn import metrics as skmetrics
 from data.data_loader import DataLoader
-from sklearn.metrics import r2_score, mean_squared_error
-from sklearn.metrics import confusion_matrix
-import matplotlib.pyplot as plot
+from model.model import Model
 import tensorflow as tf
 from tensorflow import keras
-from sklearn import preprocessing
 import datetime
 from typing import List
 
 
-class NNModel(object):
-    def __init__(self,
-                 data_loader: DataLoader,
-                 network_layers: List,
-                 **kwargs):
-        self.x_train, self.x_test, self.y_train, self.y_test = None, None, None, None
-        self.data_loader = data_loader
+class NNModel(Model):
+    def __init__(self, data_loader: DataLoader, network_layers: List, **kwargs):
+        super().__init__(data_loader)
         self.model = self.create_neural_network(network_layers, **kwargs)
-
-    def load_model_data(self,
-                        test_size=0.2,
-                        random_state=42,
-                        is_classification_problem=True,
-                        number_of_classes=10,
-                        normalize_output=False,
-                        normalize_input=False,
-                        normalize_range=None
-                        ):
-        print("Loading model data ...")
-
-        # Normalize input
-        if normalize_input:
-            if normalize_range is None:
-                normalize_range = (0, 1)
-            self.minmax_scale = preprocessing.MinMaxScaler(feature_range=normalize_range)
-            self.minmax_scale.fit(self.data_loader.all_data_y)
-            self.data_loader.all_data_x = self.minmax_scale.transform(self.data_loader.all_data_x)
-        # Normalize output
-        if normalize_output:
-            if normalize_range is None:
-                normalize_range = (0, 1)
-            self.minmax_scale = preprocessing.MinMaxScaler(feature_range=normalize_range)
-            self.minmax_scale.fit(self.data_loader.all_data_y)
-            self.data_loader.all_data_y = self.minmax_scale.transform(self.data_loader.all_data_y)
-
-        # Classification preprocessing
-        if is_classification_problem:
-            self.data_loader.all_data_y = self.preprocess_y_data(self.data_loader.all_data_y, number_of_classes)
-
-        self.x_train, self.x_test, self.y_train, self.y_test = train_test_split(self.data_loader.all_data_x,
-                                                                                self.data_loader.all_data_y,
-                                                                                test_size=test_size,
-                                                                                random_state=random_state)
-        print("Training data loaded")
 
     def train_model(self,
                     output_file_name: str,
@@ -74,7 +28,6 @@ class NNModel(object):
         # Saving file
         if save_file:
             self.save_model(f"generated_models/{output_file_name}", extension=output_file_extension)
-            print(f"Saved model in generated_models/{output_file_name}.{output_file_extension}")
 
     def save_model(self, output_file_name: str, extension: str = "h5"):
         self.model.save(f"{output_file_name}.{extension}")
@@ -84,110 +37,34 @@ class NNModel(object):
         self.model = tf.keras.models.load_model(f'{file_name}.{extension}')
         print(f'Loaded model from {file_name}.{extension}')
 
-    def evaluate_model(self,
-                       model_name: str,
-                       include_regression_metrics=True,
-                       include_classification_metrics=False,
-                       largest_classification_value=10,
-                       plot_classification_data=False,
-                       log_evaluation=True):
-        print("Evaluating model ...")
-        evaluation = []
-
-        self.model.evaluate(self.x_test, self.y_test, verbose=2)
-        self.y_pred = self.model.predict(self.x_test)
-
-        # Regression Metrics
-        if include_regression_metrics:
-            # R2 score
-            r2 = r2_score(self.y_test, self.y_pred)
-            # MSE
-            mse = mean_squared_error(self.y_test, self.y_pred)
-
-            evaluation.append(f'R2 score: {r2}\n')
-            evaluation.append(f'MSE: {mse}\n')
-
-        # Classification Metrics
-        if include_classification_metrics:
-            # In case of classification, get max index as class
-            y_pred_classification = list(map(self._get_max_index, self.y_pred))
-            y_test_classification = list(map(self._get_max_index, self.y_test))
-            # Rounding to maximum classification value
-            y_pred_classification = [round(pred) if pred <= largest_classification_value else largest_classification_value for pred in y_pred_classification]
-            # Classification metrics
-            classification_report = skmetrics.classification_report(y_test_classification, y_pred_classification)
-            classification_confusion_matrix = confusion_matrix(y_test_classification, y_pred_classification)
-
-            evaluation.append(f'Report: \n{classification_report}\n')
-            evaluation.append(f'Confusion matrix: \n{classification_confusion_matrix}\n')
-
-            if plot_classification_data:
-                plot.scatter(y_test_classification, y_pred_classification, color='red')
-                plot.xlabel('True Value')
-                plot.ylabel('Predicted Value')
-                plot.show()
-
-        # Log evaluation
-        if log_evaluation:
-            file_object = open(f'evaluation_logs/{model_name}.txt', 'a')
-            file_object.write(f"\n{model_name}\n {evaluation} \n=====================================\n")
-            file_object.close()
-
-        print(f"Evaluation:\n{evaluation}")
-
-    def predict(self, x_test):
-        y_pred = self.model.predict(x_test)
-        return y_pred
-
-    @staticmethod
-    def preprocess_y_data(y_data: np.ndarray, number_of_classes=11):
-        y_data_preprocessed = []
-
-        for y in y_data:
-            y_data_row = np.zeros(number_of_classes)
-            y_data_row[y] = 1
-            y_data_preprocessed.append(y_data_row)
-
-        # y = 3 -> y = [0 0 0 1 0 0 0 0 0 0 0 0]
-        return np.array(y_data_preprocessed)
-
     @staticmethod
     def create_neural_network(neural_network: List,
-                              optimizer="Adam",
-                              learning_rate=0.001,
-                              loss='categorical_crossentropy',
-                              metrics=None,
-                              print_summary=False):
+                              **kwargs):
+
+        optimizer = kwargs.get('optimizer', "Adam")
+        learning_rate = kwargs.get('learning_rate', 0.001)
+        loss = kwargs.get('learning_rate', 'categorical_crossentropy')
+        metrics = kwargs.get('metrics', ["accuracy"])
+        print_summary = kwargs.get('print_summary', True)
 
         # Generating nn layers
         nn_layers = []
         for layer in neural_network:
             if layer[0] == "Input":
                 nn_layers.append(tf.keras.layers.Dense(layer[1], input_shape=(layer[1],)))
-            elif layer[0] == "Denes":
+            elif layer[0] == "Dense":
                 nn_layers.append(tf.keras.layers.Dense(layer[1], activation=layer[2]))
         model = tf.keras.models.Sequential(nn_layers)
 
         # Optimizer
         if optimizer == "Adam":
-            opt = tf.keras.optimizers.Adam(learning_rate=0.001)
+            opt = tf.keras.optimizers.Adam(learning_rate=learning_rate)
         else:
             opt = optimizer
 
-        if metrics is None:
-            metrics = ["accuracy"]
         model.compile(optimizer=opt,
                       loss=loss,
                       metrics=metrics)
         if print_summary:
             model.summary()
         return model
-
-    @staticmethod
-    def _get_max_index(np_array: np.ndarray):
-        l = np_array.tolist()
-        return l.index(max(l))
-
-
-if __name__ == "__main__":
-    pass
--- model/svr_model.py
+++ model/svr_model.py
@@ -1,85 +1,41 @@
-import matplotlib.pyplot as plt
-from sklearn.model_selection import train_test_split
-from sklearn.linear_model import LogisticRegression
-import numpy as np
-from sklearn import metrics
 from data.data_loader import DataLoader
+from model.model import Model
 from sklearn.svm import SVR
 import pickle
-from sklearn.metrics import r2_score, mean_squared_error
-from sklearn.metrics import confusion_matrix
-import matplotlib.pyplot as plot
 
-class SVRModel(object):
-    def __init__(self,
-                 data_loader: DataLoader,
-                 kernel='rbf',
-                 C=100,
-                 epsilon=0.1):
-        self.x_train, self.x_test, self.y_train, self.y_test = None, None, None, None
-        self.data_loader = data_loader
-        self.model = SVR(kernel=kernel, C=C, epsilon=epsilon)
 
-    def load_training_data(self,
-                           test_size=0.2,
-                           random_state=42  # Use same state to compare models
-                           ):
-        self.x_train, self.x_test, self.y_train, self.y_test = train_test_split(self.data_loader.all_data_x,
-                                                                                self.data_loader.all_data_y,
-                                                                                test_size=test_size,
-                                                                                random_state=random_state)
+class SVRModel(Model):
+    def __init__(self, data_loader: DataLoader, **kwargs):
+        super().__init__(data_loader)
+
+        self.model = self.create_svr(kwargs)
 
     def train_model(self,
                     output_file_name: str,
-                    save_file: bool = False):
-        print('Started training model ...')
+                    output_file_extension: str = "h5",
+                    save_file: bool = False,
+                    epochs=10):
+        # Train model
         self.model.fit(self.x_train, self.y_train)
         print('Done training...')
+
+        # Saving file
         if save_file:
-            self.save_model(f"generated_models/{output_file_name}")
+            self.save_model(f"generated_models/{output_file_name}", extension=output_file_extension)
 
-    def save_model(self, output_file_name: str):
-        pickle.dump(self.model, open(f"{output_file_name}.sav", 'wb'))
-        print(f'Saved model in {output_file_name}')
+    def save_model(self, output_file_name: str, extension: str = "sav"):
+        pickle.dump(self.model, open(f"{output_file_name}.{extension}", 'wb'))
+        print(f'Saved model in {output_file_name}.{extension}')
 
-    def load_model(self, file_name: str):
-        self.model = pickle.load(open(f'{file_name}.sav', 'rb'))
-        print(f'Loaded model from {file_name}')
+    def load_model(self, file_name: str, extension: str = "sav"):
+        self.model = pickle.load(open(f'{file_name}.{extension}', 'rb'))
+        print(f'Loaded model from {file_name}.{extension}')
 
-    def evaluate_model(self,
-                       file_name: str,
-                       largest_value=10,
-                       include_classification=False,
-                       plot_data = True,
-                       log_eval = True):
-        print("Evaluating model ...")
-        y_pred = self.model.predict(self.x_test)
-        r2 = r2_score(self.y_test, y_pred)
-        mse = mean_squared_error(self.y_test, y_pred)
-        print(f'R2 score: {r2}')
-        print(f'MSE: {mse}')
+    @staticmethod
+    def create_svr(**kwargs):
+        kernel = kwargs.get('kernel', "rbf")
+        C = kwargs.get('C', 100)
+        epsilon = kwargs.get('epsilon', 0.1)
 
-        if include_classification:
-            y_pred_classification = [round(pred) if pred <= largest_value else largest_value for pred in y_pred]
-            # Classification metrics
-            classification_report = metrics.classification_report(self.y_test, y_pred_classification)
-            classification_confusion_matrix = confusion_matrix(self.y_test, y_pred_classification)
-            print(f'Report: {classification_report}')
-            print(f'Confusion matrix: {classification_confusion_matrix}')
-        if plot_data:
-            plot.scatter(self.y_test, y_pred, color='red')
-
-            plot.xlabel('True Value')
-            plot.ylabel('Predicted Value')
-            plot.show()
-        if log_eval:
-            file_object = open('logs.txt', 'a')
-            file_object.write(f"\n{file_name} \n R2 --> {r2} \n MSE --> {mse} \n=====================================\n")
-            file_object.close()
-
-    def predict(self, test_x):
-        y_pred = self.model.predict(test_x)
-        return y_pred
-
-if __name__ == "__main__":
-    pass
+        model = SVR(kernel=kernel, C=C, epsilon=epsilon)
+        return model
