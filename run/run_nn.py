bd5aa811ab39a769e5f4f7e07e400b6a9088446d
Charbel199 <charbel-boumaroun@outlook.com>
2022-04-30 00:06:59 +0300

    Clean up run nn code

--- run/run_nn.py
+++ run/run_nn.py
@@ -1,7 +1,7 @@
 from data.data_loader import DataLoader
-from model.svr_model import SVRModel
 from model.nn_model import NNModel
 from visualization import error_bars, points_cloud
+from visualization import environment_oil_thickness_distribution as e
 
 # Initial parameters
 file_name = 'thickness-9freqs-variance0.001-'
@@ -31,13 +31,13 @@ if new_model:
 else:
     model.load_model(f"generated_models/{model_name}")
 model.evaluate_model(model_name=model_name, log_evaluation=True, include_classification_metrics=False)
-
 # size_to_view = 40
 # y_pred = model.predict(model.x_test[0:size_to_view])
 # for i in range(len(y_pred)):
 #     print(f"{(model.y_test[i])} --> {(y_pred[i])}")
 
 
+# Error bars and point clouds evaluation
 save_figs = False
 selected_permittivity = 3
 observed_values = model.y_test[:, 1]
@@ -49,9 +49,7 @@ observed_values = model.y_test[:, 0]
 predicted_values = model.y_pred[:, 0]
 error_bars.generate_error_bars(observed_values, predicted_values, "Observed permittivity", "Predicted permittivity", save_fig=save_figs, output_file_name="PermittivityErrorBars")
 
-## Circle visualization
-from visualization import environment_oil_thickness_distribution as e
-
+# Circle visualization
 env = e.get_circle_thickness_distribution(size=200, smallest_thickness=1, step_size=1)
 e.visualize_environment(env)
 # populated_env_permittivity = e.fill_environment_with_reflectivity_data_2_outputs(env, data_loader=loader, model=model, is_multi_output=True, selected_permittivity=selected_permittivity)
