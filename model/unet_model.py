4acbe34705139aff07d1896bd7fef69b17e9b16a
Charbel199 <charbel-boumaroun@outlook.com>
2022-06-03 12:35:36 +0300

    Add Unet model

---
+++ model/unet_model.py
@@ -0,0 +1,65 @@
+import torch
+import torch.nn as nn
+import torchvision.transforms.functional as TF
+
+
+class DoubleConvolution(nn.Module):
+    def __init__(self, in_channels, out_channels):
+        super().__init__()
+        self.conv = nn.Sequential(
+            nn.Conv2d(in_channels, out_channels, 3, 1, 1, bias=False),  # Padding 1, Same convolution
+            nn.BatchNorm2d(out_channels),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(out_channels, out_channels, 3, 1, 1, bias=False),  # Padding 1, Same convolution
+            nn.BatchNorm2d(out_channels),
+            nn.ReLU(inplace=True)
+        )
+
+    def forward(self, x):
+        return self.conv(x)
+
+
+class UNET(nn.Module):
+    def __init__(self, in_channels=3, out_channels=1, features=None):
+        super().__init__()
+        if features is None:
+            features = [64, 128, 256, 512]
+        self.ups = nn.ModuleList()
+        self.downs = nn.ModuleList()
+        self.pool = nn.MaxPool2d(kernel_size=2, stride=2)
+
+        # Down part
+        for feat in features:
+            self.downs.append(DoubleConvolution(in_channels, feat))
+            in_channels = feat
+
+        # Up part
+        for feat in reversed(features):
+            self.ups.append(nn.ConvTranspose2d(feat * 2, feat, kernel_size=2, stride=2))
+            self.ups.append(DoubleConvolution(feat * 2, feat))
+
+        self.bottleneck = DoubleConvolution(features[-1], features[-1] * 2)
+        self.final_conv = nn.Conv2d(features[0], out_channels, kernel_size=1)
+
+    def forward(self, x):
+        skip_connections = []
+        for down in self.downs:
+            x = down(x)
+            skip_connections.append(x)
+            x = self.pool(x)
+
+        x = self.bottleneck(x)
+        skip_connections = skip_connections[::-1]
+        for index in range(0, len(self.ups), 2):
+            x = self.ups[index](x)
+            skip_connection = skip_connections[index // 2]
+
+            if x.shape != skip_connection.shape:
+                x = TF.resize(x, size=skip_connection.shape[2:])  # Just getting height and width
+            concatenate_skip = torch.cat((skip_connection, x), dim=1)
+            x = self.ups[index + 1](concatenate_skip)
+
+        return self.final_conv(x)
+
+
+
