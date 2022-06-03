8a2f121f553155318fc6a7257db1da7d2ab19e11
Charbel199 <charbel-boumaroun@outlook.com>
2022-06-03 14:14:27 +0300

    Add save as image

--- helper/numpy_helpers.py
+++ helper/numpy_helpers.py
@@ -1,9 +1,15 @@
 import numpy as np
+from PIL import Image
+
+
+def save_np_as_immage(arr, name) -> None:
+    im = Image.fromarray(arr)
+    im.save(f'{name}.jpeg')
 
 
 def save_np(arr, name):
     np.save(f'{name}.npy', arr)  # save
 
 
-def load_np(name):
+def load_np(name) -> np.ndarray:
     return np.load(f'{name}.npy')  # load
