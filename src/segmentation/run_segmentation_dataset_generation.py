9305494bb96731ed8fe063dc74afb9c72e6535a1
Charbel199 <charbel-boumaroun@outlook.com>
2022-06-17 15:18:27 +0200

    Update segmentation dataset generation

--- src/segmentation/run_segmentation_dataset_generation.py
+++ src/segmentation/run_segmentation_dataset_generation.py
@@ -10,16 +10,19 @@ from helper.numpy_helpers import *
 # ==================================================================================================================
 FILE_NAME = 'thickness-4freqs-variance0.02'
 DATA_PATH = f"assets/generated_data/variance_0.02/{FILE_NAME}"
-SHOW_REFLECTIVITIES_PLOTS = True
+SHOW_REFLECTIVITIES_PLOTS = False
 RES = (1, 1)
 OCTAVES = 2
 OUTPUT_SHAPE = (100, 100)
 SMALLEST_THICKNESS = 0
 LARGEST_THICKNESS = 10
-NUMBER_OF_DATA_POINTS = 0
+STARTING_POINT = 30
+NUMBER_OF_DATA_POINTS = 20
 FOR_TRAINING = False
 IS_CLASSIFICATION = True
-OUTPUT_FOLDER_PATH = f"assets/generated_data/variance_0.02/fractals_with_0_classification/{'training' if FOR_TRAINING else 'validation'}"
+CLASSIFICATION_ONLY = False
+INVERTED = True
+OUTPUT_FOLDER_PATH = f"assets/generated_data/variance_0.02/fractals_with_0_cascaded/{'training' if FOR_TRAINING else 'validation'}"
 # ==================================================================================================================
 
 
@@ -27,14 +30,14 @@ OUTPUT_FOLDER_PATH = f"assets/generated_data/variance_0.02/fractals_with_0_class
 loader = DataLoader()
 max_number_of_rows = 10000
 loader.load_data_from_file(
-    file_name= DATA_PATH,
+    file_name=DATA_PATH,
     file_format="{}-{}.txt",
     possible_output_values=[(0, 10, 1)],
     max_number_of_rows=max_number_of_rows)
 
 if SHOW_REFLECTIVITIES_PLOTS:
     env = generate_fractal_environment(smallest_thickness=SMALLEST_THICKNESS, largest_thickness=LARGEST_THICKNESS,
-                                       shape=OUTPUT_SHAPE, res=RES, octaves=OCTAVES)
+                                       shape=OUTPUT_SHAPE, res=RES, octaves=OCTAVES, inverted=INVERTED)
     if IS_CLASSIFICATION:
         env[env != 0] = 1
     visualize_environment(environment=env,
@@ -55,19 +58,22 @@ if SHOW_REFLECTIVITIES_PLOTS:
                           save_fig=False,
                           show_fig=True)
 
-for i in range(NUMBER_OF_DATA_POINTS):
+for i in range(STARTING_POINT, STARTING_POINT+NUMBER_OF_DATA_POINTS):
     env = generate_fractal_environment(smallest_thickness=SMALLEST_THICKNESS, largest_thickness=LARGEST_THICKNESS,
-                                       shape=OUTPUT_SHAPE, res=RES, octaves=OCTAVES)
+                                       shape=OUTPUT_SHAPE, res=RES, octaves=OCTAVES, inverted=INVERTED)
     reflect_env = fill_environment_with_reflectivity_data(environment=env, data_loader=loader)
-
-    if IS_CLASSIFICATION:
-        env[env != 0] = 1
-
     save_np(reflect_env, os.path.join(OUTPUT_FOLDER_PATH, f'x{i}'))
-    save_np(env, os.path.join(OUTPUT_FOLDER_PATH, f'y{i}'))
+    if IS_CLASSIFICATION:
+        classification_env = env.copy()
+        classification_env[classification_env != 0] = 1
+        save_np(classification_env, os.path.join(OUTPUT_FOLDER_PATH, f'yc{i}'))
+        if CLASSIFICATION_ONLY:
+            continue
+    save_np(env, os.path.join(OUTPUT_FOLDER_PATH, f'ye{i}'))
     visualize_environment(environment=env,
                           output_file_name=os.path.join(OUTPUT_FOLDER_PATH, f'y{i}'),
                           save_fig=True,
                           show_fig=False,
                           file_type='jpeg')
+
     print(f"Saved image {i}")
