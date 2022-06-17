e2530fb0c244c9350b5f4588f1d5047d8b7e7e80
Charbel199 <charbel-boumaroun@outlook.com>
2022-06-17 15:17:42 +0200

    Add inverse option for fractal environment generation

--- src/visualization/environments.py
+++ src/visualization/environments.py
@@ -1,11 +1,7 @@
 import numpy as np
 
-
-import numpy as np
-
-
 def _interpolant(t):
-    return t*t*t*(t*(t*6 - 15) + 10)
+    return t * t * t * (t * (t * 6 - 15) + 10)
 
 
 def _generate_perlin_noise_2d(
@@ -29,30 +25,30 @@ def _generate_perlin_noise_2d(
     """
     delta = (res[0] / shape[0], res[1] / shape[1])
     d = (shape[0] // res[0], shape[1] // res[1])
-    grid = np.mgrid[0:res[0]:delta[0], 0:res[1]:delta[1]]\
-             .transpose(1, 2, 0) % 1
+    grid = np.mgrid[0:res[0]:delta[0], 0:res[1]:delta[1]] \
+               .transpose(1, 2, 0) % 1
     # Gradients
-    angles = 2*np.pi*np.random.rand(res[0]+1, res[1]+1)
+    angles = 2 * np.pi * np.random.rand(res[0] + 1, res[1] + 1)
     gradients = np.dstack((np.cos(angles), np.sin(angles)))
     if tileable[0]:
-        gradients[-1,:] = gradients[0,:]
+        gradients[-1, :] = gradients[0, :]
     if tileable[1]:
-        gradients[:,-1] = gradients[:,0]
+        gradients[:, -1] = gradients[:, 0]
     gradients = gradients.repeat(d[0], 0).repeat(d[1], 1)
-    g00 = gradients[    :-d[0],    :-d[1]]
-    g10 = gradients[d[0]:     ,    :-d[1]]
-    g01 = gradients[    :-d[0],d[1]:     ]
-    g11 = gradients[d[0]:     ,d[1]:     ]
+    g00 = gradients[:-d[0], :-d[1]]
+    g10 = gradients[d[0]:, :-d[1]]
+    g01 = gradients[:-d[0], d[1]:]
+    g11 = gradients[d[0]:, d[1]:]
     # Ramps
-    n00 = np.sum(np.dstack((grid[:,:,0]  , grid[:,:,1]  )) * g00, 2)
-    n10 = np.sum(np.dstack((grid[:,:,0]-1, grid[:,:,1]  )) * g10, 2)
-    n01 = np.sum(np.dstack((grid[:,:,0]  , grid[:,:,1]-1)) * g01, 2)
-    n11 = np.sum(np.dstack((grid[:,:,0]-1, grid[:,:,1]-1)) * g11, 2)
+    n00 = np.sum(np.dstack((grid[:, :, 0], grid[:, :, 1])) * g00, 2)
+    n10 = np.sum(np.dstack((grid[:, :, 0] - 1, grid[:, :, 1])) * g10, 2)
+    n01 = np.sum(np.dstack((grid[:, :, 0], grid[:, :, 1] - 1)) * g01, 2)
+    n11 = np.sum(np.dstack((grid[:, :, 0] - 1, grid[:, :, 1] - 1)) * g11, 2)
     # Interpolation
     t = interpolant(grid)
-    n0 = n00*(1-t[:,:,0]) + t[:,:,0]*n10
-    n1 = n01*(1-t[:,:,0]) + t[:,:,0]*n11
-    return np.sqrt(2)*((1-t[:,:,1])*n0 + t[:,:,1]*n1)
+    n0 = n00 * (1 - t[:, :, 0]) + t[:, :, 0] * n10
+    n1 = n01 * (1 - t[:, :, 0]) + t[:, :, 0] * n11
+    return np.sqrt(2) * ((1 - t[:, :, 1]) * n0 + t[:, :, 1] * n1)
 
 
 def _generate_fractal_noise_2d(
@@ -86,7 +82,7 @@ def _generate_fractal_noise_2d(
     amplitude = 1
     for _ in range(octaves):
         noise += amplitude * _generate_perlin_noise_2d(
-            shape, (frequency*res[0], frequency*res[1]), tileable, interpolant
+            shape, (frequency * res[0], frequency * res[1]), tileable, interpolant
         )
         frequency *= lacunarity
         amplitude *= persistence
@@ -94,13 +90,16 @@ def _generate_fractal_noise_2d(
 
 
 def generate_fractal_environment(smallest_thickness=1, largest_thickness=10, to_int=True, shape=(300, 300), res=(2, 2),
-                                 octaves=1, persistence=0.5) -> np.ndarray:
+                                 octaves=1, persistence=0.5, inverted=False) -> np.ndarray:
     env = _generate_fractal_noise_2d(shape=shape,
                                      res=res,
                                      octaves=octaves,
                                      persistence=persistence)
+    if not inverted:
+        env = np.interp(env, (env.min(), env.max()), (smallest_thickness, largest_thickness))
+    else:
+        env = np.interp(env, (env.min(), env.max()), (largest_thickness, smallest_thickness))
 
-    env = np.interp(env, (env.min(), env.max()), (smallest_thickness, largest_thickness))
     env = np.rint(env)
     if to_int:
         env = env.astype('uint8')
