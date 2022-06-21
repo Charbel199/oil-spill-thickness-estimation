0705854e238e7c835ea22d385499760bfec86b4f
Charbel199 <charbel-boumaroun@outlook.com>
2022-06-21 14:02:52 +0200

    Add evaluate model in segmentation training

--- src/segmentation/run_segmentation_training.py
+++ src/segmentation/run_segmentation_training.py
@@ -5,31 +5,35 @@ import torch.optim as optim
 import torch
 from data.datasets.oil_environment_dataset import get_loaders, OilEnvironmentDataset
 from helper.torch_helpers import load_checkpoint, save_checkpoint
-# from model.unet_model import UNET
-from model.unet_model_classification import UNETClassifier as UNET
+from model.unet_model import UNET
+
+# from model.unet_model_classification import UNETClassifier as UNET
 
 # Parameters
 # ==================================================================================================================
 LEARNING_RATE = 1e-4
 DEVICE = "cpu"
 BATCH_SIZE = 10
-NUM_EPOCHS = 10
+NUM_EPOCHS = 20
 NUM_WORKERS = 0
 IMAGE_HEIGHT = 80  # 1280 originally
 IMAGE_WIDTH = 80  # 1918 originally
 PIN_MEMORY = True
 LOAD_MODEL_FROM_CHECKPOINT = False
+SAVE_PREDICTION_IMAGES = False
+EVALUATE_METRICS = True
 MODEL_CHECKPOINT = "my_checkpoint2.pth.tar"
-TRAIN_IMG_DIR = "assets/generated_data/variance_0.02/fractals_with_0_classification/training"
-VAL_IMG_DIR = "assets/generated_data/variance_0.02/fractals_with_0_classification/validation"
-PRED_IMG_DIR = "assets/generated_data/variance_0.02/fractals_with_0_classification/pred"
-NUM_OF_CLASSES = 1
+TRAIN_IMG_DIR = "assets/generated_data/variance_0.02/fractals_with_0/training"
+VAL_IMG_DIR = "assets/generated_data/variance_0.02/fractals_with_0/validation"
+PRED_IMG_DIR = "assets/generated_data/variance_0.02/fractals_with_0/pred"
+NUM_OF_CLASSES = 11
 SAVE = False
 LOAD = True
-MODEL_PATH = 'assets/generated_models/unet_highvariance_with_0_classification.pkl'
+MODEL_PATH = 'assets/generated_models/unet_highvariance_with_0_20epochs.pkl'
 # ==================================================================================================================
 
-model = UNET(in_channels=4, out_channels=NUM_OF_CLASSES, normalize_output=True).to(DEVICE)
+# Normalize if classification
+model = UNET(in_channels=4, out_channels=NUM_OF_CLASSES, normalize_output=False).to(DEVICE)
 train_transform = A.Compose(
     [
         A.Resize(height=IMAGE_HEIGHT, width=IMAGE_WIDTH),
@@ -57,7 +61,7 @@ val_transforms = A.Compose(
     ],
 )
 
-loss_fn = nn.BCELoss()
+loss_fn = nn.CrossEntropyLoss()
 optimizer = optim.Adam(model.parameters(), lr=LEARNING_RATE)
 
 train_loader, val_loader = get_loaders(
@@ -71,12 +75,22 @@ train_loader, val_loader = get_loaders(
     PIN_MEMORY,
 )
 
+
+def _evaluate_model():
+    # check accuracy
+    if EVALUATE_METRICS:
+        model.check_accuracy(val_loader, device=DEVICE)
+        model.evaluate_metrics(val_loader)
+    # print some examples to a folder
+    if SAVE_PREDICTION_IMAGES:
+        model.save_predictions_as_images(
+            val_loader, folder=PRED_IMG_DIR, device=DEVICE)
+
+
 if not LOAD:
 
     if LOAD_MODEL_FROM_CHECKPOINT:
         load_checkpoint(torch.load(MODEL_CHECKPOINT), model)
-
-    model.check_accuracy(val_loader, device=DEVICE)
 
     for epoch in range(NUM_EPOCHS):
         model.train_fn(train_loader, optimizer, loss_fn)
@@ -88,23 +102,14 @@ if not LOAD:
         }
         save_checkpoint(checkpoint)
 
-        # check accuracy
-        model.check_accuracy(val_loader, device=DEVICE)
+        _evaluate_model()
 
-        # print some examples to a folder
-        model.save_predictions_as_images(
-            val_loader, folder=PRED_IMG_DIR, device=DEVICE
-        )
+
 else:
     model = torch.load(MODEL_PATH)
     model.eval()
-    # check accuracy
-    model.check_accuracy(val_loader, device=DEVICE)
 
-    # print some examples to a folder
-    model.save_predictions_as_images(
-        val_loader, folder=PRED_IMG_DIR, device=DEVICE
-    )
+    _evaluate_model()
 
 if SAVE:
     torch.save(model, MODEL_PATH)
