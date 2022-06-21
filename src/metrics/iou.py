Fix iou computation

src/metrics/iou.py
@@ -2,10 +2,9 @@ import numpy as np
 
 
 def iou_coefficient(y_true, y_pred):
-
-    intersection = np.logical_and(y_true, y_pred)
+    intersection = y_true == y_pred
     union = np.logical_or(y_true, y_pred)
 
-    iou_score = np.sum(intersection) / np.sum(union)
+    iou_score = np.count_nonzero(intersection) / np.count_nonzero(union)
 
     return iou_score