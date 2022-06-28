9db4cc238244cb25dc1cb4fad2e6772dccba8657
Charbel199 <charbel-boumaroun@outlook.com>
2022-06-28 16:45:22 +0200

    Add accuracy and precision

---
+++ src/metrics/pixel_wise_accuracy.py
@@ -0,0 +1,15 @@
+from metrics.metrics_helper import pixel_wise_metrics_evaluation, get_tp, get_fp, get_fn, get_tn
+
+
+# Accuracy = TP + TN / (TP + FP + TN + FN)
+def pixel_wise_accuracy(y_true, y_pred):
+    return pixel_wise_metrics_evaluation(y_true, y_pred, _accuracy)
+
+
+def _accuracy(y_true, y_pred):
+    tp = get_tp(y_true, y_pred)
+    fp = get_fp(y_true, y_pred)
+    tn = get_tn(y_true, y_pred)
+    fn = get_fn(y_true, y_pred)
+    accuracy = (tp + tn) / (tp + fp + tn + fn)
+    return accuracy
---
+++ src/metrics/pixel_wise_precision.py
@@ -0,0 +1,13 @@
+from metrics.metrics_helper import pixel_wise_metrics_evaluation, get_tp, get_fp
+
+
+# Precision = TP / (TP + FP)
+def pixel_wise_precision(y_true, y_pred):
+    return pixel_wise_metrics_evaluation(y_true, y_pred, _precision)
+
+
+def _precision(y_true, y_pred):
+    tp = get_tp(y_true, y_pred)
+    fp = get_fp(y_true, y_pred)
+    precision = tp / (tp + fp)
+    return precision
