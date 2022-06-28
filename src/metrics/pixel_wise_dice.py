5b88d0fbd28d98cf80054be511aa60595a7293be
Charbel199 <charbel-boumaroun@outlook.com>
2022-06-28 17:02:18 +0200

    Add dice and recall

---
+++ src/metrics/pixel_wise_dice.py
@@ -0,0 +1,14 @@
+from metrics.metrics_helper import pixel_wise_metrics_evaluation, get_tp, get_fn, get_fp
+
+
+# Dice = 2*TP / (2*TP + FN + FP)
+def pixel_wise_dice(y_true, y_pred):
+    return pixel_wise_metrics_evaluation(y_true, y_pred, _dice)
+
+
+def _dice(y_true, y_pred):
+    tp = get_tp(y_true, y_pred)
+    fn = get_fn(y_true, y_pred)
+    fp = get_fp(y_true, y_pred)
+    dice = (2 * tp) / (2 * tp + fn + fp)
+    return dice
---
+++ src/metrics/pixel_wise_recall.py
@@ -0,0 +1,13 @@
+from metrics.metrics_helper import pixel_wise_metrics_evaluation, get_tp, get_fn
+
+
+# Recall = TP / (TP + FN)
+def pixel_wise_recall(y_true, y_pred):
+    return pixel_wise_metrics_evaluation(y_true, y_pred, _recall)
+
+
+def _recall(y_true, y_pred):
+    tp = get_tp(y_true, y_pred)
+    fn = get_fn(y_true, y_pred)
+    recall = tp / (tp + fn)
+    return recall
