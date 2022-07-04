d3a7a1d69df91b7d64a159225255b920dd3ca5c1
Charbel199 <charbel-boumaroun@outlook.com>
2022-07-04 13:25:27 +0200

    Update run segmentation cascaded training

--- src/segmentation/run_segmentation_cascaded_training.py
+++ src/segmentation/run_segmentation_cascaded_training.py
@@ -7,35 +7,37 @@ from data.datasets.oil_environment_dataset import get_loaders, OilEnvironmentDat
 from model.unet_model_cascaded import SemanticSegmentationCascadedModel, DiceLoss
 from model.unet_model import UNET
 from model.unet_model_classification import UNETClassifier
-from data.datasets.datasets_helpers import get_mean_and_std
+from data.datasets.datasets_helpers import get_mean_and_std, get_max
 from torch.utils.tensorboard import SummaryWriter
 
 # Parameters
 # ==================================================================================================================
-LEARNING_RATE = 1e-4
+LEARNING_RATE_COMBINED = 1e-3
+LEARNING_RATE_ESTIMATOR = 1e-3
+LEARNING_RATE_CLASSIFIER = 2e-4
 DEVICE = "cpu"
 COMPUTE_MEAN_AND_STD = False
-NORMALIZE = True
-BATCH_SIZE = 10
+NORMALIZE = False
+BATCH_SIZE = 30
 NUM_EPOCHS = 10
 NUM_WORKERS = 0
 IMAGE_HEIGHT = 80  # 1280 originally
 IMAGE_WIDTH = 80  # 1918 originally
 PIN_MEMORY = False
 LOAD_MODEL_FROM_CHECKPOINT = False
-SAVE = True
-LOAD = False
-COMBINED_LOSS = True
-SAVE_PREDICTION_IMAGES = False
+SAVE = False
+LOAD = True
+COMBINED_LOSS = False
+SAVE_PREDICTION_IMAGES = True
 EVALUATE_METRICS = True
 MODEL_CHECKPOINT = "my_checkpoint2.pth.tar"
 TRAIN_IMG_DIR = "assets/generated_data/variance_0.02/fractals_with_0_cascaded/training"
 VAL_IMG_DIR = "assets/generated_data/variance_0.02/fractals_with_0_cascaded/validation"
 PRED_IMG_DIR = "assets/generated_data/variance_0.02/fractals_with_0_cascaded/pred"
-CLASSIFIER_MODEL_PATH = 'assets/generated_models/unet_highvariance_with_0_cascaded_normalized_classifier_unified_loss_10epochs.pkl'
-ESTIMATOR_MODEL_PATH = 'assets/generated_models/unet_highvariance_with_0_cascaded_normalized_estimator_unified_loss_10epochs.pkl'
+CLASSIFIER_MODEL_PATH = 'assets/generated_models/unet_highvariance_with_0_cascaded_classifier_20epochs.pkl'
+ESTIMATOR_MODEL_PATH = 'assets/generated_models/unet_highvariance_with_0_cascaded_estimator_20epochs.pkl'
 # ==================================================================================================================
-writer = SummaryWriter("assets/logs/unet_highvariance_with_0_cascaded_normalized_unified_loss_10epochs")
+writer = SummaryWriter("assets/logs/unet_highvariance_windspeed_1_with_0_cascaded_normalized_unified_loss_10epochs")
 
 classifier = UNETClassifier(in_channels=4, out_channels=1, normalize_output=True, features=[64, 128, 256, 512]).to(
     DEVICE)
@@ -45,36 +47,70 @@ cascaded_model = SemanticSegmentationCascadedModel(classifier=classifier,
                                                    estimator=estimator)
 train_transform = []
 val_transform = []
+
+
 if not COMPUTE_MEAN_AND_STD and NORMALIZE:
+    # Variance 0.02
+    # mean = [0.4678, 0.4022, 0.4325, 0.4570]
+    # std = [0.1877, 0.1894, 0.1965, 0.1972]
+    # max_pixel_value = 1.2243
+
+    # Variance 0.02 Windspeed 1
+    mean = [0.0003, 0.0005, 0.0003, -0.0007]
+    std = [0.1417, 0.1409, 0.1416, 0.1415]
+    max_pixel_value = 0.6473100185394287
+
+    # mean = [ 0.0003,  0.0002,  0.0007, -0.0002]
+    # std = [0.1421, 0.1413, 0.1417, 0.1416]
+    # max_pixel_value = 0.6473100185394287
+
     train_transform = A.Compose(
         [
             A.Normalize(
-                mean=[0.4678, 0.4022, 0.4325, 0.4570],
-                std=[0.1877, 0.1894, 0.1965, 0.1972],
-                max_pixel_value=1,
+                mean=mean,
+                std=std,
+                max_pixel_value=max_pixel_value,
             ),
-        ]
+            A.Rotate(limit=35, p=1.0),
+        ],
+        additional_targets={'mask1': 'mask',
+                            'mask2': 'mask', }
     )
     val_transform = A.Compose(
         [
             A.Normalize(
-                mean=[0.4678, 0.4022, 0.4325, 0.4570],
-                std=[0.1877, 0.1894, 0.1965, 0.1972],
-                max_pixel_value=1.2243,
+                mean=mean,
+                std=std,
+                max_pixel_value=max_pixel_value,
             )
-        ]
+        ],
+        additional_targets={'mask1': 'mask',
+                            'mask2': 'mask', }
     )
+
+# test_transform = A.Compose(
+#     [
+#         A.Rotate(limit=35, p=1.0),
+#     ]
+# )
+# from helper.numpy_helpers import load_np
+# import os
+#
+# y_np = load_np(os.path.join(TRAIN_IMG_DIR, f"ye{5}"))
+# visualize_environment(y_np,save_fig=False)
+# y2_np = test_transform(image=y_np)['image']
+# visualize_environment(y2_np,save_fig=False)
 
 # Set criterion
 criterion_classifier = DiceLoss()
 criterion_estimator = nn.CrossEntropyLoss()
 # Set optimizers
-opt_classifier = optim.Adam(classifier.parameters(), lr=LEARNING_RATE)
-opt_estimator = optim.Adam(estimator.parameters(), lr=LEARNING_RATE)
+opt_classifier = optim.Adam(classifier.parameters(), lr=LEARNING_RATE_CLASSIFIER)
+opt_estimator = optim.Adam(estimator.parameters(), lr=LEARNING_RATE_ESTIMATOR)
 opt_all = optim.Adam([
     {'params': classifier.parameters()},
     {'params': estimator.parameters()}
-], lr=LEARNING_RATE)
+], lr=LEARNING_RATE_COMBINED)
 
 train_loader, val_loader = get_loaders(
     TRAIN_IMG_DIR,
@@ -90,17 +126,19 @@ train_loader, val_loader = get_loaders(
 if COMPUTE_MEAN_AND_STD:
     print(f"Training dataset mean and std {get_mean_and_std(train_loader)}")
     print(f"Validation dataset mean and std {get_mean_and_std(val_loader)}")
+    print(f"Training dataset max {get_max(train_loader)}")
+    print(f"Validation dataset max  {get_max(val_loader)}")
+
     exit()
 
 
 def _evaluate_model(save_images=True):
+    if EVALUATE_METRICS:
+        cascaded_model.evaluate_metrics(val_loader, device=DEVICE)
     if SAVE_PREDICTION_IMAGES and save_images:
         cascaded_model.save_predictions_as_images(
             val_loader, folder=PRED_IMG_DIR, device=DEVICE
         )
-    if EVALUATE_METRICS:
-        cascaded_model.evaluate_metrics(val_loader, device=DEVICE)
-
 
 if not LOAD:
     classifier.train()
