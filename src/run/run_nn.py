eb89702cc7cc016803cb58859983d190dcc64ec1
Charbel199 <charbel-boumaroun@outlook.com>
2022-10-08 10:07:58 +0300

    Update run nn

--- src/run/run_nn.py
+++ src/run/run_nn.py
@@ -3,7 +3,8 @@ from model.nn_model import NNModel
 from visualization import error_bars, points_cloud
 from visualization import environment_oil_thickness_distribution as e
 from visualization.environments import generate_circle_environment
-from helper.numpy_helpers import load_np
+import numpy as np
+from helper.numpy_helpers import save_np, load_np
 # Parameters
 # ==================================================================================================================
 NEW_MODEL = False
@@ -17,19 +18,21 @@ network_layers = [
 FILE_NAME = 'thickness-9freqs-variance0.02'
 DATA_PATH = f"assets/generated_data/variance_0.02_all_windspeeds/{FILE_NAME}"
 OUTPUT_FILE_NAME = 'assets/generated_models/ann_highvariance_all_windspeeds_with_0'
-PRED_IMG_DIR = "assets/generated_data/variance_0.02_all_windspeeds/fluids_cascaded_9freq/pred/ann"
+PRED_IMG_DIR = "assets/generated_data/variance_0.02_all_windspeeds/fluids_cascaded_9freq/custom_pred/ann"
 SAVE = False
 LOAD = True
 # ==================================================================================================================
 
 
 # Data
+
 # loader = DataLoader()
 # loader.load_data_from_file(
 #     file_name=f"generated_data/{FILE_NAME}",
 #     file_format="{}permittivity{}-{}.txt",
 #     possible_output_values=[(2.8, 3.3, 0.1), (1, 10, 1)],
 #     max_number_of_rows=10000)
+
 loader = DataLoader()
 possible_output_values = [(0, 10, 1)]
 max_number_of_rows = 20000
@@ -41,6 +44,7 @@ loader.load_data_from_file(
 
 
 # Training and evaluation
+
 model = NNModel(data_loader=loader, network_layers=network_layers, loss='mean_squared_error', print_summary=True)
 model.load_model_data(test_size=0.1, is_classification_problem=False, normalize_output=False)
 if NEW_MODEL:
@@ -48,23 +52,41 @@ if NEW_MODEL:
 else:
     model.load_model(f"{OUTPUT_FILE_NAME}")
 
-# model.evaluate_model(model_name=OUTPUT_FILE_NAME, log_evaluation=True, include_classification_metrics=False)
+# General evaluation
 
+# model.evaluate_model(model_name=OUTPUT_FILE_NAME, log_evaluation=True, include_classification_metrics=False)
 x_all = []
 y_all = []
-for i in range(63):
-    x_all.append(load_np(f"assets/generated_data/variance_0.02_all_windspeeds/fluids_cascaded_9freq/validation/x{i}"))
-    y_all.append(load_np(f"assets/generated_data/variance_0.02_all_windspeeds/fluids_cascaded_9freq/validation/ye{i}"))
+for i in range(2):
+    x_all.append(load_np(f"assets/generated_data/variance_0.02_all_windspeeds/fluids_cascaded_9freq/custom_training/x{i}"))
+    y_all.append(load_np(f"assets/generated_data/variance_0.02_all_windspeeds/fluids_cascaded_9freq/custom_training/ye{i}"))
     print(f"Loaded image {i}")
 
 model.evaluate_metrics(x_all, y_all, PRED_IMG_DIR)
-# size_to_view = 40
-# y_pred = model.predict(model.x_test[0:size_to_view])
-# for i in range(len(y_pred)):
-#     print(f"{(model.y_test[i])} --> {(y_pred[i])}")
 
+# Specific output evaluation
 
-# Error bars and point clouds evaluation
+x = load_np(f"assets/generated_data/variance_0.02_all_windspeeds/fluids_cascaded_9freq/validation/x14")
+ye = load_np(f"assets/generated_data/variance_0.02_all_windspeeds/fluids_cascaded_9freq/validation/ye14")
+
+x = np.array(x)
+x = np.reshape(x, (-1, 9))
+y_pred = (model.predict(x))
+y_pred = np.reshape(y_pred, (100, 100, -1))
+y_pred = np.squeeze(y_pred)
+y_pred = np.rint(y_pred)
+y_pred[y_pred > 10] = 10
+y_pred[y_pred < 0] = 0
+preds = []
+for i, row in enumerate(ye):
+    for j, point in enumerate(row):
+        if point == 3:
+            preds.append(y_pred[i][j])
+preds_np = np.array(preds)
+save_np(preds_np, 'ann_3mm')
+
+# # Error bars and point clouds evaluation
+
 # save_figs = False
 # selected_permittivity = 3
 # observed_values = model.y_test[:, 1]
@@ -75,8 +97,10 @@ model.evaluate_metrics(x_all, y_all, PRED_IMG_DIR)
 # observed_values = model.y_test[:, 0]
 # predicted_values = model.y_pred[:, 0]
 # error_bars.generate_error_bars(observed_values, predicted_values, "Observed permittivity", "Predicted permittivity", save_fig=save_figs, output_file_name="PermittivityErrorBars")
-#
+
+
 # # Circle visualization
+
 # env = generate_circle_environment(size=200, smallest_thickness=1, step_size=1)
 # e.visualize_environment(env)
 # # populated_env_permittivity = e.fill_environment_with_reflectivity_data_2_outputs(env, data_loader=loader, model=model, is_multi_output=True, selected_permittivity=selected_permittivity)
